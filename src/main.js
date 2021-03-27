90dcb9d87ba7b59da6b826d2b04f1bded13fd390
Dinis Costa <scorpaust@hotmail.com>
2021-03-27 19:06:56 +0000

    First commit.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import * as VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import HomePage from './components/HomePage.vue'
+import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
+
+const routes = [
+  { path: '/', component: HomePage }
+]
+
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes // short for `routes: routes`
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(router);
+app.mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/lib/styles/main.sass'
+
+import * as components from 'vuetify/lib/components'
+import * as directives from 'vuetify/lib/directives'
+
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  components,
+  directives,
+})
